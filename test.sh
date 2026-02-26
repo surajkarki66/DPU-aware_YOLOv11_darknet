2710456f4f9574aa74a5e441f84a88a4933b8097
Suraj Karki <suraj.karki500@protonmail.com>
2026-02-26 11:33:29 +0100

    feat: add all test scenarios

--- test.sh
+++ test.sh
@@ -3,4 +3,7 @@
 #python3 test.py --input-size 416 --version t --hyp data/hyps/args_hpd.yaml --weights runs/train_t/best.pt
 #python3 test.py --input-size 416 --version m --hyp data/hyps/args_hpd.yaml --weights runs/train_m/best.pt
 
-#python3 test.py --input-size 416 --version n --hyp data/hyps/args_hpd.yaml --task obb --weights runs/train_n/best.pt --data-dir ./coco_data_obb
+python3 test.py --input-size 416 --version n --hyp data/hyps/args_hpd.yaml --task obb --weights runs/train_n/best.pt --data-dir ./coco_data_obb
+python3 test.py --input-size 416 --version t --hyp data/hyps/args_hpd.yaml --task obb --weights runs/train_t/best.pt --data-dir ./coco_data_obb
+python3 test.py --input-size 416 --version s --hyp data/hyps/args_hpd.yaml --task obb --weights runs/train_s/best.pt --data-dir ./coco_data_obb
+python3 test.py --input-size 416 --version m --hyp data/hyps/args_hpd.yaml --task obb --weights runs/train_m/best.pt --data-dir ./coco_data_obb

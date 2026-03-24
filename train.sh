68d120a0fb65b6bffe1a28be9217bfc9c61196a5
Suraj Karki <suraj.karki500@protonmail.com>
2026-03-24 11:35:41 +0100

    add proper yaml files for each experiment

--- train.sh
+++ train.sh
@@ -11,5 +11,5 @@
 #python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version m --activation relu --hyp data/hyps/args_hpd.yaml --data-dir ./coco_data_obb
 
 # OBB
-#python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version n --activation relu --hyp data/hyps/args.yaml --data-dir ./dota.v1_large_coco
-python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version t --activation relu --hyp data/hyps/args.yaml --data-dir ./dota.v1_large_coco
+#python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version n --activation relu --hyp data/hyps/args_dota_large.yaml --data-dir ./dota.v1_large_coco
+python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version t --activation relu --hyp data/hyps/args_dota_large.yaml --data-dir ./dota.v1_large_coco

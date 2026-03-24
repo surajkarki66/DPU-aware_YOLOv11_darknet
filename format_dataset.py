851ff73b3a30db7a5bde54352a0ef33fc605543b
Suraj Karki <suraj.karki500@protonmail.com>
2026-03-24 11:18:38 +0100

    modified class nams

--- format_dataset.py
+++ format_dataset.py
@@ -3,7 +3,7 @@ import os
 from tqdm import tqdm
 
 # Define base directory
-base_dir = "./dotav1.5-coco"
+base_dir = "./dota.v1_large_coco"
 
 # Subdirectories containing images
 subdirs = ["images/train2017", "images/val2017", "images/test2017"]

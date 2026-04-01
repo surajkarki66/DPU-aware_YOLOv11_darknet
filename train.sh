bda6d319fb877bbc2602ae90ea98a187686d1ddb
Suraj Karki <suraj.karki500@protonmail.com>
2026-04-01 18:10:47 +0200

    chore(train.sh): label HPD/DOTA sections and comment default COCO command

--- train.sh
+++ train.sh
@@ -1,16 +1,16 @@
+# HPD
 #python3 train.py --input-size 416 --batch-size 32 --epochs 900 --version n --activation relu --hyp data/hyps/args_hpd.yaml
 #python3 train.py --input-size 416 --batch-size 32 --epochs 900 --version t --activation relu --hyp data/hyps/args_hpd.yaml
 #python3 train.py --input-size 416 --batch-size 32 --epochs 900 --version s --activation relu --hyp data/hyps/args_hpd.yaml
 #python3 train.py --input-size 416 --batch-size 32 --epochs 900 --version m --activation relu --hyp data/hyps/args_hpd.yaml
 
-
-# OBB
+# HPD
 #python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version n --activation relu --hyp data/hyps/args_hpd.yaml --data-dir ./coco_data_obb
 #python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version t --activation relu --hyp data/hyps/args_hpd.yaml --data-dir ./coco_data_obb
 #python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version s --activation relu --hyp data/hyps/args_hpd.yaml --data-dir ./coco_data_obb
 #python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version m --activation relu --hyp data/hyps/args_hpd.yaml --data-dir ./coco_data_obb
 
-# OBB
+# DOTA
 #python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version n --activation relu --hyp data/hyps/args_dota_large.yaml --data-dir ./dota.v1_large_coco
 #python3 train.py --task obb --input-size 416 --batch-size 32 --epochs 900 --version t --activation relu --hyp data/hyps/args_dota_large.yaml --data-dir ./dota.v1_large_coco
 #python3 train.py --task obb --input-size 1024 --batch-size 32 --epochs 900 --version n --activation relu --hyp data/hyps/args_dota_large.yaml --data-dir ./dotav1.5
@@ -22,4 +22,4 @@
 #python3 train.py --input-size 416 --batch-size 32 --epochs 900 --version n --activation relu --hyp data/hyps/args_kitti.yaml --data-dir ./kitti_COCO
 
 # COCO
-python3 train.py --input-size 416 --batch-size 32 --epochs 300 --version n --activation relu --hyp data/hyps/args_coco.yaml --data-dir ./coco
+#python3 train.py --input-size 416 --batch-size 32 --epochs 300 --version n --activation relu --hyp data/hyps/args_coco.yaml --data-dir ./coco
